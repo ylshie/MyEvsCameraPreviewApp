c66cf6ec5b2c03208654d1f3b8de134165b12d60
ylshie <yuliang.hsieh@gmail.com>
2024-07-22 14:49:54 +0800

    refactor completed

--- src/com/ylshie/android/car/evs/CarEvsCameraPreviewActivity.java
+++ src/com/ylshie/android/car/evs/CarEvsCameraPreviewActivity.java
@@ -17,20 +17,21 @@
 package com.ylshie.android.car.evs;
 
 import static android.car.evs.CarEvsManager.ERROR_NONE;
-import static android.hardware.display.DisplayManager.DisplayListener;
+//import static android.hardware.display.DisplayManager.DisplayListener;
 
 import android.app.Activity;
 import android.car.Car;
-import android.car.Car.CarServiceLifecycleListener;
-import android.car.CarNotConnectedException;
-import android.car.evs.CarEvsBufferDescriptor;
+//import android.car.Car.CarServiceLifecycleListener;
+//import android.car.CarNotConnectedException;
+//import android.car.evs.CarEvsBufferDescriptor;
 import android.car.evs.CarEvsManager;
+
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.PixelFormat;
-import android.hardware.display.DisplayManager;
+//import android.hardware.display.DisplayManager;
 import android.os.Bundle;
 
 import android.util.Log;
@@ -49,9 +50,6 @@ import com.android.car.internal.evs.GLES20CarEvsBufferRenderer;
 import android.opengl.GLSurfaceView;
 
 import java.util.ArrayList;
-
-
-
 
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -80,65 +78,6 @@ public class CarEvsCameraPreviewActivity extends EvsBaseActivity {
     private GLSurfaceView mEvsView;
     private ViewGroup mRootView;
     private LinearLayout mPreviewContainer;
-
-    /** Display manager to monitor the display's state */
-    private DisplayManager mDisplayManager;
-
-    /** Current display state */
-    private int mDisplayState = Display.STATE_OFF;
-
-    /**
-     * The Activity with showWhenLocked doesn't go to sleep even if the display sleeps.
-     * So we'd like to monitor the display state and react on it manually.
-     */
-    private final DisplayListener mDisplayListener = new DisplayListener() {
-        @Override
-        public void onDisplayAdded(int displayId) {}
-
-        @Override
-        public void onDisplayRemoved(int displayId) {}
-
-        @Override
-        public void onDisplayChanged(int displayId) {
-            if (displayId != Display.DEFAULT_DISPLAY) {
-                return;
-            }
-            int state = decideViewVisibility();
-            synchronized (mLock) {
-                mDisplayState = state;
-                handleVideoStreamLocked(state == Display.STATE_ON ?
-                        STREAM_STATE_VISIBLE : STREAM_STATE_INVISIBLE);
-            }
-        }
-    };
-
-    /** CarService status listener  */
-    private final CarServiceLifecycleListener mCarServiceLifecycleListener = (car, ready) -> {
-        try {
-            synchronized (mLock) {
-                mCar = ready ? car : null;
-                mEvsManager = ready ? (CarEvsManager) car.getCarManager(Car.CAR_EVS_SERVICE) : null;
-                if (!ready) {
-                    if (!mUseSystemWindow) {
-                        // If we were launched by the user manually, we enter the LOST state and
-                        // wait for the car service's restoration.
-                        handleVideoStreamLocked(STREAM_STATE_LOST);
-                    } else {
-                        // If we were launched by the system,we will clean up the states and
-                        // then finish; the car service will request a new instance when it comes
-                        // back from the incident while the system still requires the rearview.
-                        handleVideoStreamLocked(STREAM_STATE_STOPPED);
-                        finish();
-                    }
-                } else {
-                    // We request to start a video stream if we get connected to the car service.
-                    handleVideoStreamLocked(STREAM_STATE_VISIBLE);
-                }
-            }
-        } catch (CarNotConnectedException err) {
-            Log.e(TAG, "Failed to connect to the Car Service");
-        }
-    };
 
     private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
         @Override
@@ -181,33 +120,13 @@ public class CarEvsCameraPreviewActivity extends EvsBaseActivity {
         parseExtra(getIntent());
 
         setShowWhenLocked(true);
-        mDisplayManager = getSystemService(DisplayManager.class);
-        mDisplayManager.registerDisplayListener(mDisplayListener, null);
-        int state = decideViewVisibility();
-        synchronized (mLock) {
-            mDisplayState = state;
-        }
-
-        Log.d(TAG, "[Arthur] createCar");
-        Car.createCar(getApplicationContext(), /* handler = */ null,
-                Car.CAR_WAIT_TIMEOUT_WAIT_FOREVER, mCarServiceLifecycleListener);
-
+        
         // Packaging parameters to create CarEvsGLSurfaceView.
-        /*
-        ArrayList callbacks = new ArrayList<>(1);
-        callbacks.add(CarEvsManager.SERVICE_TYPE_REARVIEW, this);
-        int angleInDegree = getApplicationContext()
-                .getResources().getInteger(R.integer.config_evsRearviewCameraInPlaneRotationAngle);
-        */
         //Arthur
         //mEvsView = CarEvsGLSurfaceView.create(getApplication(), callbacks, getApplicationContext()
         //        .getResources().getInteger(R.integer.config_evsRearviewCameraInPlaneRotationAngle),
         //        DEFAULT_1X1_POSITION);
         //mEvsView = new GLSurfaceView(this);
-        /*
-        Log.d(TAG, "[Arthur] new GLES20CarEvsBufferRenderer");
-        render = new GLES20CarEvsBufferRenderer(callbacks, angleInDegree, DEFAULT_1X1_POSITION);
-        */
         mRootView = (ViewGroup) LayoutInflater.from(this).inflate(
                 R.layout.evs_preview_activity, /* root= */ null);
         mPreviewContainer = mRootView.findViewById(R.id.evs_preview_container);
@@ -217,37 +136,10 @@ public class CarEvsCameraPreviewActivity extends EvsBaseActivity {
                 1.0f
         );
         Log.d(TAG, "[Arthur] find view_finder");
-        //SurfaceView viewFinder = mEvsView;
+
         SurfaceView viewFinder = mPreviewContainer.findViewById(R.id.view_finder);
         Log.d(TAG, "[Arthur] createPipeline");
-        if (viewFinder != null) {
-            createPipeline(viewFinder);
-            SurfaceHolder holder = viewFinder.getHolder();
-            //Context context = this;
-            Log.d(TAG, "[Arthur] addCallback");
-            holder.addCallback(new SurfaceHolder.Callback() {
-                @Override
-                public void surfaceCreated(SurfaceHolder _holder) {
-                    Log.d(TAG, "[Arthur] surfaceCreated");
-                    //createTexture();
-                    viewFinder.post(new Runnable() {
-                        @Override
-                        public void run() {
-                            Log.d(TAG, "[Arthur] Runnable");
-                            pipeline.createResources(_holder.getSurface());
-                        }
-                    });
-                }
-                @Override
-                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-                    Log.d(TAG,"surfaceChanged");
-                }
-                @Override
-                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                    Log.d(TAG,"surfaceDestroyed");
-                }
-            });
-        }
+        hookView(viewFinder);
         //Arthur
         if (mEvsView != null) {
             mEvsView.setLayoutParams(viewParam);
@@ -341,18 +233,6 @@ public class CarEvsCameraPreviewActivity extends EvsBaseActivity {
     protected void onDestroy() {
         Log.d(TAG, "onDestroy");
         try {
-            // Request to stop current service and unregister a status listener
-            synchronized (mLock) {
-                if (mEvsManager != null) {
-                    handleVideoStreamLocked(STREAM_STATE_STOPPED);
-                    mEvsManager.clearStatusListener();
-                }
-                if (mCar != null) {
-                    mCar.disconnect();
-                }
-            }
-
-            mDisplayManager.unregisterDisplayListener(mDisplayListener);
             if (mUseSystemWindow) {
                 WindowManager wm = getSystemService(WindowManager.class);
                 wm.removeViewImmediate(mRootView);
@@ -362,21 +242,6 @@ public class CarEvsCameraPreviewActivity extends EvsBaseActivity {
         } finally {
             super.onDestroy();
         }
-    }
-
-    // Hides the view when the display is off to save the system resource, since this has
-    // 'showWhenLocked' attribute, this will not go to PAUSED state even if the display turns off.
-    private int decideViewVisibility() {
-        Display defaultDisplay = mDisplayManager.getDisplay(Display.DEFAULT_DISPLAY);
-        int state = defaultDisplay.getState();
-        Log.d(TAG, "decideShowWhenLocked: displayState=" + state);
-        if (state == Display.STATE_ON) {
-            getWindow().getDecorView().setVisibility(View.VISIBLE);
-        } else {
-            getWindow().getDecorView().setVisibility(View.INVISIBLE);
-        }
-
-        return state;
     }
 
     private void handleCloseButtonTriggered() {
--- src/com/ylshie/android/car/evs/EvsBaseActivity.java
+++ src/com/ylshie/android/car/evs/EvsBaseActivity.java
@@ -31,10 +31,15 @@ import java.util.Date;
 import java.util.Locale;
 
 import android.content.Context;
+import android.view.Display;
+
+import static android.hardware.display.DisplayManager.DisplayListener;
+import android.hardware.display.DisplayManager;
+import android.view.WindowManager;
+import android.view.View;
 
 public class EvsBaseActivity extends Activity         
         implements CarEvsGLSurfaceView.BufferCallback, HardwarePipeline.BufferCallback {
-    
     @GuardedBy("mLock")
     protected IBinder mSessionToken;
 
@@ -101,6 +106,37 @@ public class EvsBaseActivity extends Activity
         }
     }
 
+    /** Display manager to monitor the display's state */
+    private DisplayManager mDisplayManager;
+
+    /** Current display state */
+    private int mDisplayState = Display.STATE_OFF;
+
+    /**
+     * The Activity with showWhenLocked doesn't go to sleep even if the display sleeps.
+     * So we'd like to monitor the display state and react on it manually.
+     */
+    private final DisplayListener mDisplayListener = new DisplayListener() {
+        @Override
+        public void onDisplayAdded(int displayId) {}
+
+        @Override
+        public void onDisplayRemoved(int displayId) {}
+
+        @Override
+        public void onDisplayChanged(int displayId) {
+            if (displayId != Display.DEFAULT_DISPLAY) {
+                return;
+            }
+            int state = decideViewVisibility();
+            synchronized (mLock) {
+                mDisplayState = state;
+                handleVideoStreamLocked(state == Display.STATE_ON ?
+                        STREAM_STATE_VISIBLE : STREAM_STATE_INVISIBLE);
+            }
+        }
+    };
+
     /** Callback to listen to EVS stream */
     protected final CarEvsManager.CarEvsStreamCallback mStreamHandler =
             new CarEvsManager.CarEvsStreamCallback() {
@@ -154,6 +190,34 @@ public class EvsBaseActivity extends Activity
                     }
                 }
             }
+        }
+    };
+
+    /** CarService status listener  */
+    private final CarServiceLifecycleListener mCarServiceLifecycleListener = (car, ready) -> {
+        try {
+            synchronized (mLock) {
+                mCar = ready ? car : null;
+                mEvsManager = ready ? (CarEvsManager) car.getCarManager(Car.CAR_EVS_SERVICE) : null;
+                if (!ready) {
+                    if (!mUseSystemWindow) {
+                        // If we were launched by the user manually, we enter the LOST state and
+                        // wait for the car service's restoration.
+                        handleVideoStreamLocked(STREAM_STATE_LOST);
+                    } else {
+                        // If we were launched by the system,we will clean up the states and
+                        // then finish; the car service will request a new instance when it comes
+                        // back from the incident while the system still requires the rearview.
+                        handleVideoStreamLocked(STREAM_STATE_STOPPED);
+                        finish();
+                    }
+                } else {
+                    // We request to start a video stream if we get connected to the car service.
+                    handleVideoStreamLocked(STREAM_STATE_VISIBLE);
+                }
+            }
+        } catch (CarNotConnectedException err) {
+            Log.e(TAG, "Failed to connect to the Car Service");
         }
     };
 
@@ -266,7 +330,49 @@ public class EvsBaseActivity extends Activity
     }
     private final int RECORDER_VIDEO_BITRATE = 10_000_000;
 
-    protected void createPipeline(SurfaceView viewFinder) {
+    protected void hookView(SurfaceView viewFinder) {
+        if (viewFinder != null) {
+            createPipeline(viewFinder);
+            SurfaceHolder holder = viewFinder.getHolder();
+            //Context context = this;
+            Log.d(TAG, "[Arthur] addCallback");
+            holder.addCallback(new SurfaceHolder.Callback() {
+                @Override
+                public void surfaceCreated(SurfaceHolder _holder) {
+                    Log.d(TAG, "[Arthur] surfaceCreated");
+                    //createTexture();
+                    viewFinder.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Log.d(TAG, "[Arthur] Runnable");
+                            pipeline.createResources(_holder.getSurface());
+                        }
+                    });
+                }
+                @Override
+                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+                    Log.d(TAG,"surfaceChanged");
+                }
+                @Override
+                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+                    Log.d(TAG,"surfaceDestroyed");
+                }
+            });
+        }
+    }
+
+    private void createPipeline(SurfaceView viewFinder) {
+        mDisplayManager = getSystemService(DisplayManager.class);
+        mDisplayManager.registerDisplayListener(mDisplayListener, null);
+        int state = decideViewVisibility();
+        synchronized (mLock) {
+            mDisplayState = state;
+        }
+
+        Log.d(TAG, "[Arthur] createCar");
+        Car.createCar(getApplicationContext(), /* handler = */ null,
+        Car.CAR_WAIT_TIMEOUT_WAIT_FOREVER, mCarServiceLifecycleListener);
+
         ArrayList callbacks = new ArrayList<>(1);
         callbacks.add(CarEvsManager.SERVICE_TYPE_REARVIEW, this);
         int angleInDegree = getApplicationContext()
@@ -292,5 +398,49 @@ public class EvsBaseActivity extends Activity
         pipeline = new HardwarePipeline(640, 480, viewFinder, orientation, encoder);
         pipeline.setBufferCallback(this);
         pipeline.setPreviewSize(new Size(640,480));
+    }
+
+    // Hides the view when the display is off to save the system resource, since this has
+    // 'showWhenLocked' attribute, this will not go to PAUSED state even if the display turns off.
+    private int decideViewVisibility() {
+        Display defaultDisplay = mDisplayManager.getDisplay(Display.DEFAULT_DISPLAY);
+        int state = defaultDisplay.getState();
+        Log.d(TAG, "decideShowWhenLocked: displayState=" + state);
+        if (state == Display.STATE_ON) {
+            getWindow().getDecorView().setVisibility(View.VISIBLE);
+        } else {
+            getWindow().getDecorView().setVisibility(View.INVISIBLE);
+        }
+
+        return state;
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy");
+        try {
+            // Request to stop current service and unregister a status listener
+            synchronized (mLock) {
+                if (mEvsManager != null) {
+                    handleVideoStreamLocked(STREAM_STATE_STOPPED);
+                    mEvsManager.clearStatusListener();
+                }
+                if (mCar != null) {
+                    mCar.disconnect();
+                }
+            }
+
+            mDisplayManager.unregisterDisplayListener(mDisplayListener);
+        /*
+            if (mUseSystemWindow) {
+                WindowManager wm = getSystemService(WindowManager.class);
+                wm.removeViewImmediate(mRootView);
+            }
+        
+            unregisterReceiver(mBroadcastReceiver);
+        */
+        } finally {
+            super.onDestroy();
+        }
     }
 }
